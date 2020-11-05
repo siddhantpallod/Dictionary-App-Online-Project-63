8a4c08a2ff16da58af224a70fabbfc4356cc7e8f
siddhantpallod <68698846+siddhantpallod@users.noreply.github.com>
2020-11-05 10:54:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen'
+
+export default function App() {
+  return (
+    <View>
+      <HomeScreen/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
