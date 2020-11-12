48b4e059aa2e9bfc49edb794d2d4044f22e62db8
Siddhant Pallod <siddhantpallod@gmail.com>
2020-11-12 19:56:02 +0530

    Made Changes

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen'
+import {createAppContainer , createSwitchNavigator} from 'react-navigation'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <AppContainer/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+var AppNavigator = createSwitchNavigator({
+  HomeScreen : HomeScreen
+})
+
+const AppContainer = createAppContainer(AppNavigator)
+
+
